269d59d095d776eb04f0c3af53fc7996049b2ecd
Oscar <riquelme.oscar@gmail.com>
2019-02-13 20:44:39 -0300

    portafolioOscar

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,19 +6,15 @@ $(document).ready(function(){
 $(function(){
 
   $("a").click(function(event){
-    if(this.hash !==""); {
+    if(this.hash !==  "") {
       event.preventDefault();
       var gato = this.hash;
 
-      $()"html,body").animate({
+      $("html,body").animate({
         scrollTop: $(gato).offset().top
       }, 800, function(){
-
         window.location.hash = gato;
       });
     }
-    
-  }
-
 });
 });
